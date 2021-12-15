be5559d14fd520417db351aad59bf9545c9751bb
insolento <maxim.bezverkhniy@gmail.com>
2021-12-15 13:34:59 +0200

    DZ 1_3

---
+++ Navigation/Navigation/AppDelegate.swift
@@ -0,0 +1,38 @@
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        
+        let tabbarController = UITabBarController()
+        
+        let profile = ProfileViewController()
+        let profileNavigationController = UINavigationController(rootViewController: profile)
+        let profileName = UITabBarItem()
+        profileName.image = UIImage(systemName: "person.crop.circle")
+        profileName.title = "Profile"
+        profileNavigationController.tabBarItem = profileName
+        
+        let feed = FeedViewController()
+        let feedNavigationController = UINavigationController(rootViewController: feed)
+        let feedName = UITabBarItem()
+        feedName.title = "Feed"
+        feedName.image = UIImage(systemName: "house")
+        
+        let post = PostViewController()
+        
+        feedNavigationController.tabBarItem = feedName
+        
+        
+        tabbarController.viewControllers = [profileNavigationController, feedNavigationController]
+        window?.rootViewController = tabbarController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
