4ee72a168945afcb88e57a65af3dc79f07760055
insolento <maxim.bezverkhniy@gmail.com>
2022-01-05 22:15:12 +0200

    Dz 2_3

--- Navigation/Navigation/InfoViewController.swift
+++ Navigation/Navigation/InfoViewController.swift
@@ -5,23 +5,29 @@ class InfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-        
-        let alertButton: UIButton = {
-            let button = UIButton(type: .system)
-            button.frame.size.height = 75
-            button.frame.size.width = 250
-            button.center = view.center
-            button.setTitle("Open alert", for: .normal)
-            button.addTarget(self, action:#selector(self.openAlert), for: .touchUpInside)
-            return button
-        }()
-        
+        let alertButton = UIButton(type: .system)
+        alertButton.frame.size.height = 75
+        alertButton.frame.size.width = 250
+        alertButton.center = view.center
+        alertButton.setTitle("Open alert", for: .normal)
         view.addSubview(alertButton)
+        alertButton.addTarget(self, action:#selector(self.openAlert), for: .touchUpInside)
     }
     
     @objc func openAlert() {
         let alertWindow = UIAlertController(title: "Name of alert", message: "Message of Alert", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(alert: UIAlertAction!) in self.pressedCancel()})
+        let moreInfAction = UIAlertAction(title: "More Information!", style: .default, handler: {(alert: UIAlertAction!) in self.pressedMI()})
+        alertWindow.addAction(cancelAction)
+        alertWindow.addAction(moreInfAction)
         present(alertWindow, animated: true, completion: nil)
         print("Было открыто UIAlertController")
+    }
+    func pressedMI() {
+        print("More Information UIAlert")
+    }
+    
+    func pressedCancel() {
+        print("Cancel UIAlert")
     }
 }
